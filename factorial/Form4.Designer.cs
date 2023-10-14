6b8844c23570d5dfff5cf2a0bcf3123b367e2886
Daniil Tabolkin <dtalin2001@gmail.com>
2023-10-14 08:56:03 +0300

    Factorial
    Fack torial

---
+++ factorial/Form4.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace factorial
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.просмотрТToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.факториалToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(153, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Число";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Snow;
+            this.button1.Location = new System.Drawing.Point(135, 203);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 114);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 171);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Snow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(131, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Факториал";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Snow;
+            this.button2.Location = new System.Drawing.Point(297, 278);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(379, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.BackColor = System.Drawing.Color.White;
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.просмотрТToolStripMenuItem,
+            this.факториалToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // просмотрТToolStripMenuItem
+            // 
+            this.просмотрТToolStripMenuItem.Name = "просмотрТToolStripMenuItem";
+            this.просмотрТToolStripMenuItem.Size = new System.Drawing.Size(195, 24);
+            this.просмотрТToolStripMenuItem.Text = "Просмотр текста";
+            this.просмотрТToolStripMenuItem.Click += new System.EventHandler(this.просмотрТToolStripMenuItem_Click);
+            // 
+            // факториалToolStripMenuItem
+            // 
+            this.факториалToolStripMenuItem.Name = "факториалToolStripMenuItem";
+            this.факториалToolStripMenuItem.Size = new System.Drawing.Size(195, 24);
+            this.факториалToolStripMenuItem.Text = "Информация";
+            this.факториалToolStripMenuItem.Click += new System.EventHandler(this.факториалToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(276, 114);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 25);
+            this.button3.TabIndex = 8;
+            this.button3.Text = " i";
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form4";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Factorial";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem просмотрТToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem факториалToolStripMenuItem;
+    }
+}
+
---
+++ factorial/Form4.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//рассчитываем факториал заданного числа.
+//18.10.22
+namespace factorial
+{
+    public partial class Form4 : Form
+    {
+        //задаём переменные nom и fact.
+        int nom;
+        //long fact = 1;
+        int fact = 1;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+        //кнопка для преобразования.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //начало с единицы.
+            fact = 1;
+            //создаём строку для отражения её в textbox
+            string ss = "Не рассчитывается";
+            //оператор try-catch помогает обойти ошибки.
+            try
+            {
+                //преобразуем значение nom в эквивалентное 32-битное целое число со знаком.
+                nom = Convert.ToInt32(textBox1.Text);
+            }
+            //не допускает ошибку FormatException.
+            catch (FormatException)
+            {
+            }
+            //не допускает ошибку OverflowException.
+            catch (OverflowException)
+            {
+            }
+               //цикл for нужен для перебора значений.
+               for (int i = 1; i <= nom; i++)
+               {
+                   //рассчёт факториала.
+                   fact = fact * i;
+                   //отобржает факториал во втором текстовом боксе.
+                   textBox2.Text = "" + fact;
+                   //если номер больше 33 то в текстовом боксе 2
+                   //отображается строка ss"Не рассчитывается".
+                   if (nom >= 33) textBox2.Text = "" + ss.ToString();
+                   //во избежание ошибок остановить рассчёт.
+                   if (nom >= 33) break;
+                   //записываются данные в блокнот.
+                   File.WriteAllText("factorial1.txt", "Расчёт факториала." + 
+                       Environment.NewLine + "Вводимое число: " + nom + ";" + 
+                       Environment.NewLine + "Факториал равен: " + fact + ";" + 
+                       Environment.NewLine + DateTime.Now);
+               }
+    }
+        //кнопка выхода.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //кнопка i.
+        private void button3_Click(object sender, EventArgs e)
+        {
+          MessageBox.Show("Для расчёта факториала" + "\n" + "введите число от 0 до 32.");
+        }
+        //убрать отображение символов, букв и т.д.
+        private void textBox1_KeyDown(object sender, KeyPressEventArgs e)
+        {
+            //задаём строку с числом от 0 до 9.
+            string ss = "0123456789";
+            //
+            if (!ss.Contains(e.KeyChar)&& e.KeyChar!=Convert.ToChar(Keys.Back))
+            {
+                e.Handled = true; 
+            }
+        }
+        //ok отвечает на enter/exit отвечает на esc.
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //ещё один вариант:
+            //if (e.KeyValue == (char)Keys.Enter)
+            if (e.KeyValue == Convert.ToChar(Keys.Enter))
+            {
+                button1_Click(button1, null);
+            }
+            if (e.KeyValue == Convert.ToChar(Keys.Escape))
+            {
+                button2_Click(button2, null);
+            }
+        }
+        //esc.
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == Convert.ToChar(Keys.Escape))
+            {
+                button2_Click(button2, null);
+            }
+        }
+        //просмотр текста.
+        private void просмотрТToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //создаю блокнот со всеми имеющимися данными.
+        }
+        //информация об факториале.
+        private void факториалToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
